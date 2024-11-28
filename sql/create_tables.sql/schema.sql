d7bc28b9b770840b4e84f14fa73b8d3a111ba2a5
Marcelo Gomes <mars.gomes2003@gmail.com>
2024-11-28 17:08:40 +0000

    tables

---
+++ sql/create_tables.sql/schema.sql
@@ -0,0 +1,27 @@
+-- psql -h db -p 5432 postgres -d project2db
+-- Table for the DBInfo topic
+CREATE TABLE DBInfo (
+    id SERIAL PRIMARY KEY,
+    route_supplier TEXT NOT NULL,
+    route_capacity INTEGER NOT NULL
+);
+
+-- Table for the Routes topic
+CREATE TABLE Routes (
+    id SERIAL PRIMARY KEY,
+    origin TEXT NOT NULL,
+    destination TEXT NOT NULL,
+    passenger_capacity INTEGER NOT NULL,
+    transport_type TEXT CHECK (transport_type IN ('Bus', 'Taxi', 'Train', 'Metro', 'Scooter')) NOT NULL,
+    operator_name TEXT NOT NULL
+);
+
+-- Table for the Trips topic
+CREATE TABLE Trips (
+    id SERIAL PRIMARY KEY,
+    route_id INTEGER REFERENCES Routes(id),
+    origin TEXT NOT NULL,
+    destination TEXT NOT NULL,
+    passenger_name TEXT NOT NULL,
+    transport_type TEXT CHECK (transport_type IN ('Bus', 'Taxi', 'Train', 'Metro', 'Scooter')) NOT NULL
+);
